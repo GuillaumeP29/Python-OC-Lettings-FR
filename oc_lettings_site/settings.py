c2e0bbba637b7c17b6974c665411f609c3375143
Guillaume Postec <gg.postec@laposte.net>
2022-06-09 15:35:49 +0200

    Feature/Heroku setting

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', default='default_local_secret_key')  # in .env
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
